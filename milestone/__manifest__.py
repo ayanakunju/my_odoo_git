5aeeaf5f974399ddc9101c178774c4e27a9422cb
Ayana <ayanakunju202@gmail.com>
2024-09-09 19:12:31 +0530

    sep 5 updates

--- milestone/__manifest__.py
+++ milestone/__manifest__.py
@@ -4,13 +4,13 @@
     'version': '17.0.3.0.0',
     'depends': ['base', 'sale', 'project'],
     'author': "Author Name",
+    'sequence': 5,
     'category': 'Category',
     'description': """
         This module gives you a quick view of the milestone of your records
     """,
 
     'data': [
-        'security/ir.model.access.csv',
         'views/sale_order_line_views.xml',
 
     ],
--- milestone/models/sale_order_line.py
+++ milestone/models/sale_order_line.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
-from asyncio import create_task
 
-from wheel.cli.convert import parse_wininst_info
-
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class SaleOrderLine(models.Model):
@@ -14,22 +11,61 @@ class SaleOrderLine(models.Model):
 
 
 class SaleOrder(models.Model):
+    """model SaleOrder is used to inherit the sale order and create task and project based on the conditions"""
     _inherit = 'sale.order'
 
-    def action_create_project(self):
-        """action create project is to create project and task when clicking the create project button"""
-        project = self.env['project.project']
-        task = self.env['project.task']
-        for order in self:
-            project = project.create({'name': order.name})
+    project_created = fields.Boolean(string='Project Created')
 
-            lines_by_milestone = {}
-            for line in order.order_line:
-                milestone = line.milestone or 0
-                if milestone not in Q:
-                    lines_by_milestone[milestone] = []
-                    lines_by_milestone[milestone].append(line)
-                    for lines in lines_by_milestone.items():
-                        parent_task_name = f"Milestone {milestone}"
-                        parent_task = task.create({'name': parent_task_name, 'project_id': project.id})
-        return
+
+    def action_create_project(self):
+        for order in self:
+            project = self.env['project.project'].create({
+                'name': order.name,
+                'sale_order_id': order.id,
+            })
+            self._create_tasks_for_project(project)
+            self.project_created = True
+
+
+    def action_update_project(self):
+        for order in self:
+            existing_project = self.env['project.project'].search([('name', '=', order.name)])
+            existing_task = self.env['project.task'].search([('project_id','=',order.name)])
+            if existing_project:
+                existing_task.unlink()
+                existing_project.unlink()
+            self.action_create_project()
+
+    def _create_tasks_for_project(self, project):
+        milestones = {}
+        for line in self.order_line:
+            if line.milestone:
+                milestone_name = f"Milestone {line.milestone}"
+                if milestone_name not in milestones:
+                    milestones[milestone_name] = []
+                milestones[milestone_name].append(line.name)
+
+        for milestone_name, products in milestones.items():
+            print(milestones.items,"ss")
+            parent_task = self.env['project.task'].create({
+                'name': milestone_name,
+                'project_id': project.id,
+            })
+            for product in products:
+                self.env['project.task'].create({
+                    'name': f"{milestone_name} - {product}",
+                    'project_id': project.id,
+                    'parent_id': parent_task.id,
+                })
+
+    def action_get_project(self):
+        """ action_get_project function is for view the corresponding projects
+        that are created inside the sale order """
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'project',
+            'view_mode': 'kanban,form,tree',
+            'res_model': 'project.project',
+            'context': "{'create': False}"
+        }
